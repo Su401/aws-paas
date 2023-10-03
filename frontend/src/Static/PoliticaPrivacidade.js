0617a027b14cb1ccb3b4642f113f56d99045f45a
Ana Rita Gomes Cristovao Dagnino <argcd.iefp@alunos.ipb.pt>
2023-10-03 17:05:54 +0100

    changes to file

--- frontend/src/Static/PoliticaPrivacidade.js
+++ frontend/src/Static/PoliticaPrivacidade.js
@@ -2,11 +2,11 @@ import "../css/TemplatePrimario.css"
 
 export default function PoliticaPrivacidade() {
     return (
-
-        <div class="body">
+        <div className="PoliticaPrivacidade">
+            <div className="body">
                 <h1>Política de Privacidade</h1>
-                <section class="boas-vindas">
-                    <article class="primeiro">
+                <section className="boas-vindas">
+                    <article className="primeiro">
                         <p> A PAAS - Gestão de HSE e HACCP (a seguir denominada "PAAS", "nós", "nosso" ou "nossa") leva
                             a sua privacidade muito a sério. Esta Política de Privacidade e Cookies descreve como
                             coletamos, usamos e protegemos as informações pessoais que você nos fornece quando usa nosso
@@ -16,76 +16,76 @@ export default function PoliticaPrivacidade() {
                 <section>
                     <h2>1. Informações Pessoais Coletadas</h2>
                     <article>
-                        <p class="subcapitulo">1.1. Informações de Contato:</p>
-                        <p class="texto">Quando você entra em contato conosco ou se inscreve para receber nossos
+                        <p className="subcapitulo">1.1. Informações de Contato:</p>
+                        <p className="texto">Quando você entra em contato conosco ou se inscreve para receber nossos
                             serviços, podemos coletar informações pessoais, como seu nome, endereço de e-mail, número de
                             telefone e outras informações de contato relevantes.</p>
-                        <p class="subcapitulo">1.2. Informações de Navegação:</p>
-                        <p class="texto">Coletamos informações sobre como você interage com nosso site, incluindo seu
+                        <p className="subcapitulo">1.2. Informações de Navegação:</p>
+                        <p className="texto">Coletamos informações sobre como você interage com nosso site, incluindo seu
                             endereço IP, navegador da web, páginas acessadas, tempos de acesso e URLs de referência.</p>
                     </article>
                     <h2>2. Uso de Informações Pessoais</h2>
                     <article>
-                        <p class="subcapitulo">2.1. Fornecimento de Serviços:</p>
-                        <p class="texto">Utilizamos suas informações pessoais para fornecer serviços, responder a
+                        <p className="subcapitulo">2.1. Fornecimento de Serviços:</p>
+                        <p className="texto">Utilizamos suas informações pessoais para fornecer serviços, responder a
                             consultas, enviar comunicações relevantes e melhorar nossos serviços.
                         </p>
-                        <p class="subcapitulo">2.2. Cookies e Tecnologias Semelhantes:</p>
-                        <p class="texto">Utilizamos cookies e tecnologias semelhantes para personalizar sua experiência
+                        <p className="subcapitulo">2.2. Cookies e Tecnologias Semelhantes:</p>
+                        <p className="texto">Utilizamos cookies e tecnologias semelhantes para personalizar sua experiência
                             de navegação, analisar o uso do site e fornecer publicidade direcionada. Para obter mais
                             informações sobre cookies, consulte nossa seção de Política de Cookies abaixo.
                         </p>
                     </article>
                     <h2>3. Compartilhamento de Informações Pessoais</h2>
                     <article>
-                        <p class="texto">Não vendemos, alugamos ou divulgamos suas informações pessoais a
+                        <p className="texto">Não vendemos, alugamos ou divulgamos suas informações pessoais a
                             terceiros, a menos que seja necessário para fornecer nossos serviços, cumprir obrigações
                             legais ou com o seu consentimento.
                         </p>
                     </article>
                     <h2>4. Política de Cookies</h2>
                     <article>
-                        <p class="subcapitulo">4.1. O que são Cookies:</p>
-                        <p class="texto">Cookies são pequenos arquivos de texto armazenados em seu dispositivo quando
+                        <p className="subcapitulo">4.1. O que são Cookies:</p>
+                        <p className="texto">Cookies são pequenos arquivos de texto armazenados em seu dispositivo quando
                             você visita nosso site. Eles nos permitem reconhecê-lo e rastrear suas atividades online.
                         </p>
-                        <p class="subcapitulo">4.2. Tipos de Cookies:</p>
-                        <p class="texto">Utilizamos os seguintes tipos de cookies: </p>
-                        <p class="texto"><strong>Cookies Essenciais:</strong> Esses cookies são necessários para que o
+                        <p className="subcapitulo">4.2. Tipos de Cookies:</p>
+                        <p className="texto">Utilizamos os seguintes tipos de cookies: </p>
+                        <p className="texto"><strong>Cookies Essenciais:</strong> Esses cookies são necessários para que o
                             site funcione
                             corretamente. Eles incluem, por exemplo, cookies que permitem que você faça login em áreas
                             seguras do site.</p>
-                        <p class="texto"><strong>Cookies de Desempenho:</strong> Esses cookies coletam informações sobre
+                        <p className="texto"><strong>Cookies de Desempenho:</strong> Esses cookies coletam informações sobre
                             como os visitantes usam nosso site, como quais páginas são mais populares. Eles nos ajudam a
                             melhorar a funcionalidade do site.</p>
-                        <p class="texto"><strong>Cookies de Publicidade: </strong> Esses cookies são usados para
+                        <p className="texto"><strong>Cookies de Publicidade: </strong> Esses cookies são usados para
                             fornecer anúncios que sejam mais relevantes para você e seus interesses. Eles também são
                             usados para limitar o número de vezes que você vê um anúncio e para medir a eficácia das
                             campanhas publicitárias.</p>
                     </article>
                     <article>
-                        <p class="subcapitulo">4.3. Cookies de Terceiros:</p>
-                        <p class="texto">Nosso site pode incluir conteúdo de terceiros, como plug-ins de redes sociais.
+                        <p className="subcapitulo">4.3. Cookies de Terceiros:</p>
+                        <p className="texto">Nosso site pode incluir conteúdo de terceiros, como plug-ins de redes sociais.
                             Esses terceiros podem definir cookies quando você interage com seu conteúdo em nosso site.
                             Não temos controle sobre esses cookies. Consulte a política de privacidade dos terceiros
                             para obter mais informações.</p>
                     </article>
                     <h2>5. Consentimento</h2>
                     <article>
-                        <p class="texto">Ao usar nosso site e serviços, você concorda com a coleta, uso e
+                        <p className="texto">Ao usar nosso site e serviços, você concorda com a coleta, uso e
                             compartilhamento de suas informações pessoais de acordo com esta Política de Privacidade e
                             Cookies.
                         </p>
                     </article>
                     <h2>6. Alterações na Política de Privacidade <br /> e Cookies</h2>
                     <article>
-                        <p class="texto">Reservamo-nos o direito de fazer alterações nesta Política de Privacidade e
+                        <p className="texto">Reservamo-nos o direito de fazer alterações nesta Política de Privacidade e
                             Cookies a qualquer momento. A versão mais recente será sempre disponibilizada em nosso site.
                         </p>
                     </article>
                     <h2>7. Contato</h2>
                     <article>
-                        <p class="texto">Se você tiver alguma dúvida sobre esta Política de Privacidade e Cookies ou
+                        <p className="texto">Se você tiver alguma dúvida sobre esta Política de Privacidade e Cookies ou
                             desejar exercer seus direitos de proteção de dados, entre em contato conosco <a
                                 href="./contactos.html">aqui</a>.
                         </p>
@@ -93,5 +93,6 @@ export default function PoliticaPrivacidade() {
                 </section>
 
             </div>
+        </div>
     )
 }
