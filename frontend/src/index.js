532045a03c6fe3cf2e4a7ef75174abb564305cf4
Ana Rita Gomes Cristovao Dagnino <argcd.iefp@alunos.ipb.pt>
2023-10-17 23:05:03 +0100

    changes to ConsultaTarefa.js Backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import App from './App';
+import SecondaryNav from './Components/SecondaryNav/SecondaryNav';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
@@ -12,7 +13,7 @@ import './Components/Sass/my-bootstrap.scss';
 // import Contactos from './Static/Contactos';
 // import PrimaryNav from '../src/Components/PrimaryNav/PrimaryNav';
 // import Footer from '../src/Components/Footer/Footer';
-import ConsultaTarefas from './Static/ConsultaTarefas';
+import ConsultaTarefas from './Backoffice/ConsultaTarefas';
 // import GerirRegistos from './Backoffice/Registos/GerirRegistos';
 // import ImprimirRegistos from './Backoffice/Registos/ImprimirRegistos';
 // import GerirRegistos from './Backoffice/GerirRegistos';
@@ -22,6 +23,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
 	<React.StrictMode>
+		<SecondaryNav />
 		<ConsultaTarefas />
 	</React.StrictMode>
 );
