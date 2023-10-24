4bbfbbe29db17452c55382eda5498eeaea54e24b
Ana Rita Gomes Cristovao Dagnino <argcd.iefp@alunos.ipb.pt>
2023-10-24 12:31:49 +0100

    Changes to file according to mockup

--- frontend/src/Backoffice/UserComp/RegistoTarefas/Limpezas/BtnRegistoLimpezas/BtnRegistoLimpezas.js
+++ frontend/src/Backoffice/UserComp/RegistoTarefas/Limpezas/BtnRegistoLimpezas/BtnRegistoLimpezas.js
@@ -25,45 +25,34 @@ export default function BtnRegistoLimpezas() {
         <div className="ConsultaTarefas backgroundSec" >
             <div className="container">
                 <div className="container-title">
-                    <h1>Consulta de Tarefas</h1>
+                    <h1>Registo de Tarefas</h1>
                 </div>
                 <div className="btn-container">
                     <div className="row mt-3">
                         <div className="col-12 col-md-12 col-btn">
-                            <a href="#">
-                                <button className="btn btn-primary shadow-sm btn-lg" type="button">Trocar o Óleo</button>
+                            <a href="/registo-limpeza-balcao">
+                                <button className="btn btn-primary shadow-sm btn-lg" type="button">Balcão</button>
                             </a>
                         </div>
                         <div className="col-12 col-md-12 col-btn">
-                            <a href="#">
-                                <button className="btn btn-primary shadow-sm btn-lg" type="button">Limpeza da Copa</button>
+                            <a href="/registo-limpeza-sala">
+                                <button className="btn btn-primary shadow-sm btn-lg" type="button">Sala</button>
                             </a>
                         </div>
                         <div className="col-12 col-md-12 col-btn">
-                            <a href="#">
-                                <button className="btn btn-primary shadow-sm btn-lg" type="button">Limpeza da
-                                    Cozinha</button>
+                            <a href="registo-limpeza-copa">
+                                <button className="btn btn-primary shadow-sm btn-lg" type="button">Copa</button>
                             </a>
                         </div>
                         <div className="col-12 col-md-12 col-btn">
-                            <a href="./registarProdutos.html">
-                                <button className="btn btn-primary shadow-sm btn-lg" type="button">Registo de
-                                    Produto</button>
+                            <a href="registo-limpeza-cozinha">
+                                <button className="btn btn-primary shadow-sm btn-lg" type="button">Cozinha</button>
                             </a>
                         </div>
                     </div>
                 </div>
 
-                <div className="btn-bottom-container">
-                    <div className="row">
-                        <div className="col-12 col-md-12 col-btn">
-                            <a href="#">
-                                <button className="btn btn-primary m-3 m-md-5 area_col typography" type="button">Área do
-                                    Colaborador</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                
 
             </div>
         </div>
