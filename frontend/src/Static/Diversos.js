80e5bddd75ba7bf87466149b33e254b4a05b72db
Ana Rita Gomes Cristovao Dagnino <argcd.iefp@alunos.ipb.pt>
2023-10-25 22:57:23 +0100

    create Diversos.js file

---
+++ frontend/src/Static/Diversos.js
@@ -0,0 +1,44 @@
+import './TemplatePrimario.css';
+
+export default function Diversos() {
+    console.log('diversos');
+    return (
+        <div className='Diversos'>
+            <div className='body'>
+                <h1>Atendendo a Diversos Setores</h1>
+
+                <section>
+                    <article>
+
+                        <p className='texto'>
+                            Na PAAS, estamos na vanguarda da revolução digital em Gestão de Segurança, Saúde e Ambiente (HSE) e Análise de Perigos e Pontos Críticos de Controlo (HACCP). Enraizados na nossa missão de simplificar e revolucionar, inicialmente focalizamo-nos na indústria de restauração. Contudo, a verdadeira inovação revela-se na adaptabilidade das nossas soluções, que transcendem fronteiras industriais.</p>
+
+                        <h2>Começando na Restauração:</h2>
+                        <p className='texto'>Ao nos concentrarmos inicialmente na indústria de restauração, entendemos as nuances críticas e os desafios únicos associados à segurança alimentar. Desenvolvemos soluções pioneiras para gerir com eficácia os requisitos de HSE e HACCP nesse ambiente exigente.</p>
+
+                        <h2>Adaptabilidade Transcendental:</h2>
+
+                        <p className='texto'>No entanto, o nosso compromisso não se limita às cozinhas e salas de jantar. As nossas soluções são altamente adaptáveis e aplicáveis a uma variedade de setores. Seja você um gestor de um restaurante movimentado, uma fábrica industrial, uma instalação de saúde avançada ou qualquer outro tipo de negócio, estamos aqui para ajudar.</p>
+
+                        <h2>Inovação em Segurança e Conformidade:</h2>
+
+                        <p className='texto'>Na PAAS, abraçamos uma abordagem inovadora e tecnologicamente avançada para a gestão de HSE e HACCP. A digitalização dos serviços essenciais destaca-nos como pioneiros, permitindo que as empresas atinjam os mais altos padrões de segurança e conformidade regulatória.</p>
+
+                        <h2>Simplificando Complexidades:</h2>
+
+                        <p className='texto'>A nossa missão é simplificar as complexidades associadas à segurança, saúde e conformidade ambiental. Ao proporcionar soluções intuitivas e eficazes, capacitamos as empresas a eliminar obstáculos, melhorar a eficiência operacional e garantir a conformidade com os padrões globais em constante evolução.</p>
+
+                        <h2>Alcançando a Excelência:</h2>
+
+                        <p className='texto'>Junte-se a nós na vanguarda da excelência em segurança e conformidade. Seja bem-vindo à PAAS, onde a revolução digital encontra a gestão responsável, e onde a inovação tecnológica impulsiona o futuro da sua empresa.</p>
+
+                        <h3>Estamos ansiosos para ser o seu parceiro de confiança na jornada para garantir a qualidade e a segurança dos seus produtos e operações. Na PAAS, o futuro da gestão empresarial responsável começa aqui.</h3>
+
+                    </article>
+
+                </section>
+
+            </div>
+        </div>
+    );
+}
