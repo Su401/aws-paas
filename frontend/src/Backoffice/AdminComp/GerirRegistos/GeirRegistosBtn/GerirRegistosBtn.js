b74d957b6c6207ac6d1ad96ba0c9743eb374b77b
Andre Manuel Cordeiro Tavares <amct.iefp@alunos.ipb.pt>
2023-10-26 14:37:06 +0100

    gerir registos boxes

--- frontend/src/Backoffice/AdminComp/GerirRegistos/GeirRegistosBtn/GerirRegistosBtn.js
+++ frontend/src/Backoffice/AdminComp/GerirRegistos/GeirRegistosBtn/GerirRegistosBtn.js
@@ -1,11 +1,91 @@
-import React from 'react';
-
-function GerirRegistosBtn(props) {
+import './GerirRegistosBtn.css';
+export default function GerirRegistosBtn() {
 	return (
 		<div>
-			<button>Gerir registos yoo</button>
+			<div className='row'>
+				<div className='mt-lg-4'>
+					<span></span>
+				</div>
+				<div className='col greetings'>
+					<span className='hello'>Olá </span>
+					<span className='user'>Susana !</span>
+				</div>
+			</div>
+			<div className='container-fluid'>
+				<div className='row'>
+					<div className='mb-lg-5 mb-md-5 mb-sm-5'></div>
+				</div>
+				<div className='d-flex justify-content-evenly'>
+					<div className='row mt-lg-5 mt-md-5'>
+						<div className='col-lg-4'>
+							<div
+								className='card pb-lg-5'
+								style={{ width: '22rem', height: '15rem' }}
+							>
+								<div className='card-body'>
+									<a
+										href='/gerir-registos'
+										className='stretched-link btnedit'
+									>
+										<span className='letterEdit'>
+											GERIR EQUIPAMENTOS
+										</span>
+										<br></br>
+										<span className='letter2Edit'>
+											{' '}
+											DE FRIO E TROCA DE ÓLEO
+										</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='col-lg-4'>
+							<div
+								className='card pb-lg-5'
+								style={{ width: '22rem', height: '15rem' }}
+							>
+								<div className='card-body'>
+									<a
+										href='/gerir-registos'
+										className='stretched-link btnedit'
+									>
+										<span className='letterEdit'>
+											GERIR REGISTOS
+										</span>
+										<br></br>{' '}
+										<span className='letter2Edit'>
+											{' '}
+											DE LIMPEZA
+										</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className='col-lg-4'>
+							<div
+								className='card pb-lg-5'
+								style={{ width: '22rem', height: '15rem' }}
+							>
+								<div className='card-body'>
+									<a
+										href='/gerir-registos'
+										className='stretched-link btnedit'
+									>
+										<span className='letterEdit'>
+											GERIR REGISTOS
+										</span>
+										<br></br>{' '}
+										<span className='letter2Edit'>
+											{' '}
+											DE UTILIZADOR
+										</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
-
-export default GerirRegistosBtn;
